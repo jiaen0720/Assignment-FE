340010ab5507f4e3409dede541129f96d10f8d6e
jiaen0720 <khorjiaen20072001@gmail.com>
2024-08-09 16:24:51 +0800

    9/8/2024 - Completed recommendation

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,8 +33,11 @@ const navbar = document.querySelector('.navbar');
 window.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
         navbar.classList.add('shadow-sm');
+        navbar.classList.replace('bg-transparent', 'bg-white');
     } else {
         navbar.classList.remove('shadow-sm');
+        navbar.classList.replace('bg-white', 'bg-transparent');
+
     }
 });
 
